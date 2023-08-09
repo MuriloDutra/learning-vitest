dd470e99cbf82169a5f93006a173c63bde883fc1
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2023-08-09 17:10:41 -0300

    Mocking network request with MSW

---
+++ test/network.test.ts
@@ -0,0 +1,23 @@
+import { test, expect, vi, beforeAll, afterAll } from "vitest";
+import { getPostBody } from "../src/network";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+
+const server = setupServer(
+  rest.get("https://jsonplaceholder.typicode.com/posts/1", (req, res, ctx) => {
+    const id = req.params.id;
+    return res(
+      ctx.json({
+        body: "Mocked! Request id:" + id,
+      })
+    );
+  })
+);
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+
+test("should fetch", async () => {
+  const result = await getPostBody(1);
+  expect(result).toMatchInlineSnapshot('"Mocked! Request id:undefined"');
+});
