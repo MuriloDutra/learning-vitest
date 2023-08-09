1bd0b8450799814a263808b2f18280c45c9430d7
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2023-08-09 14:23:46 -0300

    Spying multiple function calls and also using Snapshots

--- test/spying.test.ts
+++ test/spying.test.ts
@@ -1,10 +1,58 @@
 import { test, expect, vi } from "vitest";
 import { greeting } from "../src/greeting";
 
-test("greeting", () => {
+test("greeting - with mock.calls", () => {
   const spy = vi.spyOn(console, "log");
-  greeting("Murilo");
 
-  expect(spy).toBeCalledWith("Hello, Murilo!");
+  greeting("Murilo");
+  greeting("Alves");
+
+  expect(spy).toBeCalledTimes(2);
+  expect(spy.mock.calls[0][0]).toBe("Hello, Murilo!");
+  expect(spy.mock.calls[1][0]).toBe("Hello, Alves!");
+});
+
+test("greeting - with mockReset", () => {
+  const spy = vi.spyOn(console, "log");
+
+  greeting("Murilo");
   expect(spy).toBeCalledTimes(1);
+  expect(spy).toBeCalledWith("Hello, Murilo!");
+  expect(spy).toMatchInlineSnapshot(`
+    [MockFunction log] {
+      "calls": [
+        [
+          "Hello, Murilo!",
+        ],
+      ],
+      "results": [
+        {
+          "type": "return",
+          "value": undefined,
+        },
+      ],
+    }
+  `);
+
+  spy.mockReset();
+
+  greeting("Alves");
+  expect(spy).toBeCalledTimes(1);
+  expect(spy).toBeCalledWith("Hello, Alves!");
+
+  expect(spy).toMatchInlineSnapshot(`
+    [MockFunction log] {
+      "calls": [
+        [
+          "Hello, Alves!",
+        ],
+      ],
+      "results": [
+        {
+          "type": "return",
+          "value": undefined,
+        },
+      ],
+    }
+  `);
 });
